1309b9da1ca3dcc4070adfe741041589fa612af9
tesslin <tesslin@bitbucket.org>
2018-02-21 15:43:55 +0530

    initial_bet_codeforces

---
+++ Initial_bet_codeforces.cpp
@@ -0,0 +1,18 @@
+//http://codeforces.com/problemset/problem/478/A
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a[5],sum=0;
+    for(int i=0;i<5;i++)
+    {
+      cin>>a[i];
+      sum+=a[i];
+}
+    if(sum%5==0&&sum>=5)
+    cout<<sum/5;
+    else
+    cout<<"-1";
+ 
+    return 0;
+}
