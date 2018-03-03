60f343c88023119538fc80e4d963e3e9ac2a90ac
tesslin <tesslin@bitbucket.org>
2018-03-03 15:28:27 +0530

    Game-513A

---
+++ Game-513 A.cpp
@@ -0,0 +1,13 @@
+//http://codeforces.com/problemset/problem/513/A
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a,b,c,d;
+    scanf("%d %d %d %d",&a,&b,&c,&d);
+    if(a>b)
+        printf("First");
+    else
+        printf("Second");
+    return 0;
+}
