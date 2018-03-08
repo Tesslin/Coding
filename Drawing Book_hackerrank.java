161f1feeb552fef93195fad88b1b1864c17a660c
tesslin <tesslin@bitbucket.org>
2018-03-08 23:39:42 +0530

    Drawing Book_hackerrank

---
+++ Drawing Book_hackerrank.java
@@ -0,0 +1,24 @@
+//https://www.hackerrank.com/challenges/drawing-book/problem
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static int solve(int n, int p){
+        // Complete this function
+        int pagesFromFront=p/2;
+        int pagesFromBack =n/2-p/2;
+        return(Math.min(pagesFromFront,pagesFromBack));
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int p = in.nextInt();
+        int result = solve(n, p);
+        System.out.println(result);
+    }
+}
