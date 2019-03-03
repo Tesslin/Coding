64dbf647d875872c8caee2085923f9cf275ec5c5
tesslin <tesslin@bitbucket.org>
2019-03-03 20:25:09 +0530

    Again Twenty Five! 630A

---
+++ againtwentyfive.java
@@ -0,0 +1,11 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class main {
+    public static void main(String []args){
+        Scanner sc=new Scanner(System.in);
+//        System.out.println("Enter string");
+        BigInteger num=sc.nextBigInteger();
+        System.out.println("25");
+    }
+}
